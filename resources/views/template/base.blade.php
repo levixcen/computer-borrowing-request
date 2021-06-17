d9e8c53bd8901f77aeb52bf614ebd80a6cfca011
christ0208 <christopherlimawan@gmail.com>
2021-06-17 14:04:21 +0700

    Fix bug for page title in window title

--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@if(empty($title)) {{ $title . ' - ' }} @endif{{ config('app.name') }}</title>
+    <title>@unless(empty($title)) {{ $title . ' - ' }} @endunless{{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
 </head>
