ac1a0517ee67c47209ee4ce175b4dcdd6c4ba483
christ0208 <christopherlimawan@gmail.com>
2021-06-23 21:11:06 +0700

    Added Filter for ScheduleController.php

--- app/Http/Controllers/Admin/ScheduleController.php
+++ app/Http/Controllers/Admin/ScheduleController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Room;
 use App\Models\Schedule;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
@@ -13,10 +16,49 @@ class ScheduleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $schedules = Schedule::query();
+
+        if ($request->description !== null) {
+            $schedules = $schedules->where('description', 'LIKE', "%{$request->description}%");
+        }
+
+        if ($request->room !== null) {
+            $schedules = $schedules->where('room_id', $request->room);
+        }
+
+        if ($request->start_date !== null) {
+            $startDate = CarbonImmutable::parse($request->start_date);
+        }
+
+        if ($request->end_date !== null) {
+            $endDate = CarbonImmutable::parse($request->end_date);
+        }
+
+        if (isset($startDate) && isset($endDate)) {
+            $startDate = $startDate->startOfDay();
+            $endDate = $endDate->endOfDay();
+
+            $schedules = $schedules->where([
+                ['start_datetime', '<=', $endDate],
+                ['end_datetime', '>=', $startDate],
+            ]);
+        } else if (isset($startDate)) {
+            $startDate = $startDate->startOfDay();
+
+            $schedules = $schedules->where('end_datetime', '>=', $startDate);
+        } else if (isset($endDate)) {
+            $endDate = $endDate->endOfDay();
+
+            $schedules = $schedules->where('start_datetime', '<=', $endDate);
+        }
+
+        $schedules = $schedules->orderByDesc('start_datetime')->paginate(10);
+
         return view('admin.schedule.index', [
-            'schedules' => Schedule::orderByDesc('start_datetime')->paginate(10),
+            'schedules' => $schedules,
+            'rooms' => Room::all(),
         ]);
     }
 
--- resources/views/admin/schedule/index.blade.php
+++ resources/views/admin/schedule/index.blade.php
@@ -3,9 +3,62 @@
 @section('contents')
     <div class="w-full">
         @include('components.admin-header')
+        <div class="container w-full max-w-3xl bg-white rounded mx-auto mt-4 p-3 shadow-md" id="filter-container">
+            <div class="flex flex-row">
+                <span class="font-bold text-xl">Filter</span>
+            </div>
+            <form action="{{ route('admin.schedules.index') }}">
+                <div>
+                    <label class="font-bold block text-gray-700 my-2" for="description">
+                        Description
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring" id="description" name="description" type="text" placeholder="Description" value="{{ request()->has('description') ? request()->get('description') : (old('description') ?? '') }}">
+                </div>
+                <div>
+                    <label class="font-bold block text-gray-700 my-2" for="room">
+                        Room
+                    </label>
+                    <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring" name="room" id="room">
+                        <option value="">-- Choose One --</option>
+                        @foreach($rooms as $room)
+                            <option value="{{ $room->id }}" @if((request()->has('room') ? request()->get('room') : (old('room') ?? '')) === $room->id) selected @endif>{{ $room->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="flex">
+                    <div class="flex-1 mr-1">
+                        <label class="font-bold block text-gray-700 my-2" for="start-date">
+                            Start Date
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring" id="start-date" name="start_date" type="date" placeholder="Start Date" value="{{ request()->has('start_date') ? request()->get('start_date') : (old('start_date') ?? '') }}">
+                    </div>
+                    <div class="flex-1 ml-1">
+                        <label class="font-bold block text-gray-700 my-2" for="end-date">
+                            End Date
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring" id="end-date" name="end_date" type="date" placeholder="End Date" value="{{ request()->has('end_date') ? request()->get('end_date') : (old('end_date') ?? '') }}">
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" class="btn bg-blue-500 text-white rounded mt-4 mr-2 p-2 hover:bg-blue-700">
+                        Filter
+                    </button>
+                    <a href="{{ route('admin.schedules.index') }}" class="ml-2 text-blue-500 underline hover:text-blue-700">
+                        Reset
+                    </a>
+                </div>
+            </form>
+        </div>
         <div class="container w-full max-w-3xl bg-white rounded mx-auto mt-4 p-3 shadow-md">
-            <div>
-                <span class="font-bold text-xl">Schedule Management</span>
+            <div class="flex flex-row">
+                <div class="flex-1">
+                    <span class="font-bold text-xl">Schedule Management</span>
+                </div>
+                <div>
+                    <button class="btn bg-blue-500 rounded px-2 py-1 text-white hover:bg-blue-700" id="btn-filter">
+                        Filter
+                    </button>
+                </div>
             </div>
             <table class="w-full border mt-4 shadow-md">
                 <thead>
@@ -38,3 +91,29 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        let filterState = false;
+
+        const changeFilterContainerState = () => {
+            if (filterState) {
+                document.getElementById('filter-container').classList.remove('hidden');
+            } else {
+                document.getElementById('filter-container').classList.add('hidden');
+            }
+        };
+
+        const onFilterClicked = () => {
+            filterState = !filterState;
+            changeFilterContainerState();
+        };
+
+        const onDocumentReady = () => {
+            changeFilterContainerState();
+            document.getElementById('btn-filter').onclick = onFilterClicked;
+        };
+
+        document.addEventListener('DOMContentLoaded', onDocumentReady);
+    </script>
+@endpush
