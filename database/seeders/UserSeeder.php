2ef2e9888314a6e58e8374b14f8acf03c5532231
christ0208 <christopherlimawan@gmail.com>
2021-06-24 10:19:09 +0700

    Added Seeder for users.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id' => '2562a09a-9d33-4c2a-9b90-6508bc18e154',
+                'name' => 'User',
+                'role' => 'User',
+                'email_verified_at' => now(),
+                'password' => Hash::make('useruser'),
+            ],
+            [
+                'id' => 'd7c8a5f0-4e1d-46b1-9068-f52e27941764',
+                'name' => 'Admin',
+                'role' => 'Administrator',
+                'email_verified_at' => now(),
+                'password' => Hash::make('adminadmin'),
+            ],
+        ];
+    }
+}
