5251af4edc9a3854b20af99bbc4c2b4f9c3fc3ea
christ0208 <christopherlimawan@gmail.com>
2021-06-24 10:21:11 +0700

    Fix seeder for user.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -19,6 +20,7 @@ class UserSeeder extends Seeder
                 'id' => '2562a09a-9d33-4c2a-9b90-6508bc18e154',
                 'name' => 'User',
                 'role' => 'User',
+                'email' => 'user@codepository.org',
                 'email_verified_at' => now(),
                 'password' => Hash::make('useruser'),
             ],
@@ -26,9 +28,12 @@ class UserSeeder extends Seeder
                 'id' => 'd7c8a5f0-4e1d-46b1-9068-f52e27941764',
                 'name' => 'Admin',
                 'role' => 'Administrator',
+                'email' => 'admin@codepository.org',
                 'email_verified_at' => now(),
                 'password' => Hash::make('adminadmin'),
             ],
         ];
+
+        DB::table('users')->insert($data);
     }
 }
